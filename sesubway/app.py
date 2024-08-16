f9faf9e2f8693de08c2414d31c0ff4a3519cd2bf
composedly13 <composedly13@gmail.com>
2024-08-17 00:29:59 +0900

    Add files via upload

--- sesubway/app.py
+++ sesubway/app.py
@@ -12,26 +12,25 @@ def home():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        # 폼 데이터 가져오기
-        username = request.json['username']
-        email = request.json['email']
-        password = request.json['password']
-            
-        # 사용자가 이미 존재하는지 확인
+        data = request.get_json()
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+
         for user in users:
             if user['email'] == email:
                 return jsonify({'error': 'User already exists!'}), 400
-        
-        # 새로운 사용자를 목록에 추가
+
         users.append({
             'username': username,
             'email': email,
-            'password': password  # 참고: 실제 애플리케이션에서는 항상 비밀번호를 해시 처리하세요!
+            'password': password
         })
 
         return jsonify({'message': 'User registered successfully!'}), 201
 
     return render_template('signup.html')
+
 
 @app.route('/favorites')
 def favorites():
